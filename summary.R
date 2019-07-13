fa86e374606ac1fe5473cbb916ef519b55dd3a05
Wang <wangx172@STAXWang172MBP2.local>
2019-07-13 09:05:53 +0800

    result of P

--- summary.R
+++ summary.R
@@ -4,3 +4,13 @@ result1 = do.call("rbind",resulttest1)
 summary(result1)
 
 sqrt(mean((result1[,2] - 0.1)^2))
+
+
+load("~/Research/FDA_subgroup_sparse/result/resulttestP1.rdata")
+resultP1 = do.call("rbind",resulttestP1)
+summary(resultP1)
+
+sqrt(mean((resultP1[,2] - 0.1)^2))
+
+summary(resultP1[,3])
+sum(resultP1[,3]==2)
